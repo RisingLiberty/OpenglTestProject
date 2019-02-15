8155a1f890abdc87f5961792842017b7f4f286f5
Nick De Breuck <nick.debreuck@outlook.com>
2019-02-15 07:51:11 +0100

    Added SDL test window

--- OpenglTestProject/OpenglTestProject/main.cpp
+++ OpenglTestProject/OpenglTestProject/main.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
 
 #include <SFML/Window.hpp>
+#include <SDL/SDL.h>
+#include <SDL/SDL_opengl.h>
 
+#undef main
 
 int main()
 {
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+	//SDL_WINDOW_OPENGL: Create a window ready for OpenGL
+	//SDL_WINDOW_RESIZABLE: create a resizable window.
+	//SDL_WINDOW_FULLSCREEN: <Optional> Create a fullscreen window.
+	SDL_Window* pWindow = SDL_CreateWindow("OpenGL", 100, 100, 800, 800, SDL_WINDOW_OPENGL);
+
+	SDL_GLContext context = SDL_GL_CreateContext(pWindow);
+
+	SDL_Event windowEvent;
+	while (true)
+	{
+		if (SDL_PollEvent(&windowEvent))
+		{
+			if (windowEvent.type == SDL_QUIT) break;
+		}
+		SDL_GL_SwapWindow(pWindow);
+	}
+
+	SDL_GL_DeleteContext(context);
+
+	return 0;
+
 	sf::ContextSettings settings;
 
 	//Equivalent to DXGO_FORMAT_D24_UNORM_S8_UNORM
@@ -41,4 +72,9 @@ int main()
 			}
 		}
 	}
+
+
+	SDL_Quit();
+
+	return 0;
 }
